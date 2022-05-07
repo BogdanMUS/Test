e698d958f8cf344e44ec9af0daae681c5e29abb6
BogdanMUS <92291814+BogdanMUS@users.noreply.github.com>
2022-05-07 15:28:31 +0500

    Add files via upload

---
+++ indexforma.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html lang="ru">
+    <head>
+        <link rel="stylesheet" href="css1.css" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+        <link
+            rel="stylesheet"
+            href="https://unpkg.com/swiper@8/swiper-bundle.min.css"
+        />
+
+        <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+    </head>
+
+    <body>
+        <div class="text__bloc">
+            <div class="container">
+                <h5>ПРОЙДИ БЕСПЛАТНУЮ ДИАГНОСТИКУ</h5>
+                <form action="send.php" method="post">
+                    <input
+                        type="text"
+                        name="fio"
+                        placeholder="Укажите ФИО"
+                        required
+                    />
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Укажите e-mail"
+                        required
+                        maxlength="60"
+                    />
+                    <input type="submit" value="Отправить" />
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
--- send.php
+++ send.php
@@ -1,17 +1,21 @@
 <?php
-$fio = $_POST['fio'];
-$email = $_POST['email'];
-$fio = htmlspecialchars($fio);
-$email = htmlspecialchars($email);
-$fio = urldecode($fio);
-$email = urldecode($email);
-$fio = trim($fio);
-$email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
- {     echo "сообщение успешно отправлено";
-} else {
-    echo "при отправке сообщения возникли ошибки";
-}?>
+    if($_POST)['fio']
+    {
+        $fio = 'Укажите ФИО';
+    }
+
+    $to = "bogdanmusnik@yandex.ru";
+    $from = trim($_POST['email']);
+
+    $headers = "From: $from" . "\r\n" .
+    "Reply-to: $from" . "\r\n" .
+    "X-Mailer: PHP/" . phpversion();
+
+    if(mail($to, $fio, $headers))
+    {
+        echo 'Письмо отправлено';
+    }else
+    {
+        echo 'Письмо  не отправлено';
+    }
+?>
